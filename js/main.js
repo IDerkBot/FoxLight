5457d5b3795b77fd9e4e6360edc3eeaa804f26ec
IDerk <ivanderk2003@gmail.com>
2020-11-28 12:15:23 +0300

    anchor

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ auth.addEventListener('click', event => {
 close.addEventListener('click', event => {
     ovForm.classList.add("none");
 });
+
+var btnUp = document.querySelector("#btn-up");
+function backToTop(){
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+    }
+};
+function trackScroll(){
+    var scrolled = window.pageYOffset;
+    if(scrolled > 10) btnUp.classList.remove('done');
+    if(scrolled < 10) btnUp.classList.add('done');
+};
+
+btnUp.addEventListener('click', backToTop);
+window.addEventListener('scroll', trackScroll);
